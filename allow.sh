e97c332038be69771646a9fbc3379b8bd77bf59a
pambudi-dedi <72483941+pambudi-dedi@users.noreply.github.com>
2020-10-08 20:55:43 +0700

    Update allow.sh

--- allow.sh
+++ allow.sh
@@ -1,1 +1,27 @@
-ipset
+
+ipset add iix 51.79.194.97
+ipset add iix 47.241.77.2
+ipset add iix 34.101.164.180
+ipset add iix 139.99.88.255
+ipset add iix 192.168.4.253
+ipset add iix 192.168.4.254
+ipset add iix 11.11.11.1
+ipset add iix 10.148.0.3
+ipset add iix 47.241.77.2
+ipset add iix 35.240.220.176
+ipset add iix 35.240.220.176
+ipset add iix 192.168.6.0/24
+#################
+ipset add iix 182.30.129.0/24
+ipset add iix 182.30.204.0/24
+#################################
+ipset add iix 103.31.251.109
+ipset add iix 36.88.62.47
+#################### DNS ########
+ipset add iix 8.8.8.8
+ipset add iix 8.8.4.4
+ipset add iix 213.186.33.99
+####################################
+ipset add iix 192.168.3.0/24
+ipset add iix 192.168.4.0/24
+
